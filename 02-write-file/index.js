ea458af5f5897bb91a02d3987dcfc13bec250233
Danil Chesnokov <shaytann831@gmail.com>
2025-01-20 17:49:10 +0300

    feat: add 02-write-file solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const readline = require('readline')
+const path = require('path')
+
+const filePath = path.join(__dirname, 'output.txt')
+
+const writableStream = fs.createWriteStream(filePath, { flags: 'a' })
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+console.log('Enter your text or "exit"\n')
+
+rl.on('line', (input) => {
+  if (input.toLowerCase() === 'exit') {
+    console.log('Bye!\n')
+    rl.close()
+    process.exit()
+  }
+
+  writableStream.write(input + '\n')
+  console.log('Enter your text or "exit" one more time')
+})
+
+process.on('SIGINT', () => {
+  console.log('\nBye!')
+  rl.close()
+  process.exit()
+})
